ebe968381e39fafb8169233e959b7bea39a944ef
luizfelipemr <luizfmrego@gmail.com>
2017-11-05 19:25:03 -0200

    Flag -lm to link with mah.h added

--- src/main.c
+++ src/main.c
@@ -6,12 +6,59 @@
  */
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+ int prime_number(int var){
+
+   int cond;
+   float aux2, aux;
+
+   if(var==1 || var ==0){
+     return 0;
+   }
+
+   //Fermat primality test
+   //This test have pseudoprimes, like 561
+
+   else{
+
+     aux = (var%2)+1;
+     aux2 = var-1;
+     cond = (int)pow(aux,aux2);
+
+     if(cond%var==1){
+       return 1;
+     }
+
+   }
+
+ }
 
 int main() {
 
-  int x, y;
+  int x;
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+  FILE *pipe;
+  pipe = popen("sort -g -n","w");
+
+  while(1){
+
+
+    scanf("%d", &x);
+
+    if(x<0){
+      break;
+    }
+
+    else{
+      if(prime_number(x)==0){
+        fprintf(pipe, "%d\n",x);
+      }
+    }
+  }
+
+  pclose(pipe);
+
   return 0;
 }
